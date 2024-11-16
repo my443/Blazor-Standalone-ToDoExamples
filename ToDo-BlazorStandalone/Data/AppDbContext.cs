18747bd61e7ce3e10d0fcdfe9c385dc4540e19a5
John van Dijk <fortyfour-three@teksavvy.com>
2024-11-16 07:41:28 -0500

    Add project files.

---
+++ ToDo-BlazorStandalone/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ToDo.Client.Models;
+
+namespace ToDo.Client.Data
+{
+    public class AppDbContext
+    {
+        public List<ToDoItem> ToDoItems { get; set; }
+
+        public AppDbContext()
+        {
+            ToDoItems = new List<ToDoItem> { 
+                new ToDoItem { Id = 1, Title = "First Item", IsDone = false },
+                new ToDoItem { Id = 2, Title = "First Item", IsDone = false },
+                new ToDoItem { Id = 3, Title = "First Item", IsDone = false },
+                new ToDoItem { Id = 4, Title = "First Item", IsDone = false },
+                                        };
+        }
+    }
+}
---
+++ ToDo-BlazorStandalone/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.Client.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details{ get; set; }
+        public bool IsDone { get; set; }
+    }
+}
