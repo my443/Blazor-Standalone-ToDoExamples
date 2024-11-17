8c77a3830a04953acd3ff53e73d71484bae21e67
John van Dijk <fortyfour-three@teksavvy.com>
2024-11-17 07:12:52 -0500

    Made the test data dynamic.

--- ToDo-BlazorStandalone/Data/AppDbContext.cs
+++ ToDo-BlazorStandalone/Data/AppDbContext.cs
@@ -8,16 +8,34 @@ namespace ToDo.Client.Data
 
         public AppDbContext()
         {
-            ToDoItems = new List<ToDoItem> { 
-                new ToDoItem { Id = 1, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 16) },
-                new ToDoItem { Id = 2, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 16) },
-                new ToDoItem { Id = 3, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 16) },
-                new ToDoItem { Id = 4, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 16) },
-                new ToDoItem { Id = 5, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 15) },
-                new ToDoItem { Id = 6, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 15) },
-                new ToDoItem { Id = 7, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 17) },
-                new ToDoItem { Id = 8, Title = "First Item", IsDone = false, ItemDate = new DateOnly(2024, 11, 17) },
-                                        };
+
+            ToDoItems = GenerateTestItems();
+
+        }
+
+        private List<ToDoItem> GenerateTestItems()
+        {
+            List<ToDoItem> ToDoItems = new List<ToDoItem>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                ToDoItem item = new ToDoItem { Id = i + 1, Title = "First Item", IsDone = false, ItemDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-1)) };
+                ToDoItems.Add(item);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                ToDoItem item = new ToDoItem { Id = i + 5, Title = "First Item", IsDone = false, ItemDate = DateOnly.FromDateTime(DateTime.Now) };
+                ToDoItems.Add(item);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                ToDoItem item = new ToDoItem { Id = i + 10, Title = "First Item", IsDone = false, ItemDate = DateOnly.FromDateTime(DateTime.Now.AddDays(1)) };
+                ToDoItems.Add(item);
+            }
+
+            return ToDoItems;
         }
     }
 }
